bc5c5343e6c4d5f7606aa362e9320e7b30b961a9
Esrael Zerihun <esraelzerihun3@gmail.com>
2026-04-21 16:54:32 +0300

    oop

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from auth import login, logout, register
 def main():
     print("\nWelcome back ==^== ")
     while True:
-        options = int(input("\n1. Login 2. Register 3. Exit\n>> "))
+        options = int(input("\n1. Login 2. Register 3. Logout\n>> "))
         if options == 1:
             login(username="", password="")
         elif options == 2:
